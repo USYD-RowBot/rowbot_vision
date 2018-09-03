7d97964aa75190f80ddd6b97714a40c12f60357c
acenturyandabit <steeven.liu2@gmail.com>
2018-09-04 08:24:19 +1000

    objectserver rehash

--- scripts/ImageServer.py
+++ scripts/ImageServer.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 from rowbot_vision.srv import *
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from vision_scripts.shapes import get_shape
-from vision_scripts.light_buoy import LightPattern
+from visops import runOperation
+from vision_scripts import shapes
+from vision_scripts import buoy
+from vision_scripts import light_buoy
+
+
+max_angle=35
+interest_region_width=0.1 ## 2 * percentagewise width of the region that we're going to be interested in 
+
 def nothing(x):
     pass
 
@@ -19,13 +26,23 @@ def overlay(bg,img2):
     img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
     dst = cv2.add(img1_bg,img2_fg)
     return dst
+
+def getContoursFromMask(mask):
+    # kernel = np.ones((5,5),np.uint8)
+    # closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    closing=mask
+    cnts = cv2.findContours(closing, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[1]
+    return cnts
+
 class ImageServer():
     def __init__(self):
         #Initalise the image server
         #Initalise image subscriber
         self.pub = rospy.Publisher("image_out",Image)
-        self.lp = LightPattern()
-        self.cvimage = None
+        self.lp = []
+        self.fimgs = {} # filtered images.
+        self.masks ={} # masks and respective contours. Masks are single channel. {'img':mask image,'cnts': contours}.
         self.image= None
         #self.image_pub = rospy.Publisher("image_topic_2", Image)
         self.bridge = CvBridge()
@@ -34,37 +51,36 @@ class ImageServer():
         pass
     def classify_buoy(self,bearing,objInFront):
         print("classifying")
-        try:
-            pass
-            #rospy.wait_for_service('detect_buoy', timeout = 0.25)
-        except rospy.ROSException, e:
-            print("Waited too long")
-        if self.image is not None:
-            try:
-                detect_buoy_service = rospy.ServiceProxy('detect_buoy',BuoyDetect)
-                req = BuoyDetectRequest()
-                req.image = self.image
-                req.bearing = bearing
-                req.ObjInFront = objInFront
-                resp = detect_buoy_service(req)
-                types = resp.types
-                conf = resp.confidences
-                print("Service returned", types,conf)
-                return types,conf
+        
+        # colours to look for: 
+        # Red
+        # white
+        # green
+        # black
 
-            except rospy.ServiceException, e:
-                print "Service call failed: %s"%e
-                return False
-        else:
-            return False
-        #print(min_water, max_water)
-        #height,width,depth = image.shape
-        #margin = margin_deg  * width/ 70
-        #x_coord = width/70 * bearing + width/2
-        #if bearing > fov/2 or bearing  < -fov/2:
-        #    print("Out of range")
-        #    return False
-        #print(x_coord)
+        img_halfwidth=self.fimgs['bgr'].shape[1]/2
+        ROI_center = (bearing / max_angle * img_halfwidth)+ img_halfwidth 
+        real_irw=interest_region_width*img_halfwidth
+
+        result=runOperation(self.masks,{
+                'sp_green':'green',
+                'sp_red':'red',
+                'sat_green':'green',
+                'sat_red':'red',
+                'black':'black',
+                'white':'white'
+            },buoy.identify, buoy.collate)
+        # rospy.loginfo(result)
+        # do a bit of post processing of the result to get it into the form we want
+        # list comprehension yay
+        print(result)
+        filtered_result=[i for i in result if abs(i['cx']-ROI_center)<real_irw]
+
+        types = [i['name'] for i in filtered_result]
+        confs = [i['conf'] for i in filtered_result]
+        retval=[types, confs]
+        
+        return retval 
 
 
 
@@ -73,39 +89,79 @@ class ImageServer():
         seconds_start = rospy.get_time()
         self.image = data
         try:
-            self.cvimage = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.fimgs['bgr'] = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
-        height, width = self.cvimage.shape[:2]
+        height, width = self.fimgs['bgr'].shape[:2]
         new_w = int(width* 1)
         new_h = int(height* 1)
-        resized = cv2.resize(self.cvimage,(new_w, new_h))
-        sd_image, result = get_shape(resized)
-        lp_image,_ = self.lp.getPattern(resized)
+        self.fimgs['bgr'] = cv2.resize(self.fimgs['bgr'],(new_w, new_h))
+        # create filtered images.
+        
+        # hsv: normal HSV.
+        self.fimgs['hsv']=cv2.cvtColor(self.fimgs['bgr'],cv2.COLOR_BGR2HSV)
+        
+        # s_hsv: shifted HSV.
+        # convert to integer so we can use modulo
+        fullh=(self.fimgs['hsv'][:,:,0]).astype(int)
+        # shift the colours by 180/12 so the red bins end up together
+        fullh+=int(180/12)
+        # loop the values back to within  0 - 180
+        fullh%=180
+        # reinsert into hsv -- for future refinement
+        self.fimgs['s_hsv']=np.copy(self.fimgs['hsv'])
+        self.fimgs['s_hsv'][:,:,0]=fullh*1.0
 
+        # brite: relative saturation and value thresholding, to get rid of grey objects and darker objects.
+        self.masks['brite']={'img':cv2.inRange(self.fimgs['s_hsv'], (0,np.max(self.fimgs['s_hsv'][:,:,1])*0.3,np.max(self.fimgs['s_hsv'][:,:,2])*0.2), (180,255,255))}
+        
+        # white and black masks
+        # white: high value, low saturation
+        self.masks['white']={'img':cv2.inRange(self.fimgs['s_hsv'], (0,0,np.max(self.fimgs['s_hsv'][:,:,2])*0.9), (180,np.max(self.fimgs['s_hsv'][:,:,1])*0.2,255))}
+        # black: low value
+        self.masks['black']={'img':cv2.inRange(self.fimgs['s_hsv'], (0,0,0),(180,255,np.max(self.fimgs['s_hsv'][:,:,2])*0.2))}
 
-        detect_buoy_service = rospy.ServiceProxy('detect_buoy',BuoyDetect)
-        req = BuoyDetectRequest()
-        req.image = self.image
-        req.bearing = 0
-        req.ObjInFront = 0
-        resp = detect_buoy_service(req)
-        #self.pub.publish(resp.mask)
-        bd_mask = self.bridge.imgmsg_to_cv2(resp.mask, "bgr8")
-        #cv2.imshow("mask",mask)
-        #print("img")
-        combined = resized.copy()
-        combined = overlay(combined,bd_mask)
+        # saturation + HSV Red, blue and green.
+        self.fimgs['brite_hsv'] = cv2.bitwise_and(self.fimgs['s_hsv'],self.fimgs['s_hsv'],mask = self.masks['brite']['img'])
+        self.masks['sat_red'] = {'img':cv2.inRange(self.fimgs['brite_hsv'],  np.array([0, 1, 1]),np.array([30, 255, 255]))}
+        self.masks['sat_green'] = {'img':cv2.inRange(self.fimgs['brite_hsv'],  np.array([50, 1, 1]),np.array([95, 255, 255]))}
+        self.masks['sat_blue'] = {'img':cv2.inRange(self.fimgs['brite_hsv'],  np.array([106, 1, 1]),np.array([140, 255, 255]))}
+        
+        # sp_green: specifically for detecting green things by making a mask which is not green.
+        # use the same theory to detect things which are definiteyl red and blue?
+        self.masks['sp_blue']={'img':255-np.clip(self.fimgs['bgr'][:,:,0]-((self.fimgs['bgr'][:,:,1]+self.fimgs['bgr'][:,:,2])/2),0,255)}
+        self.masks['sp_green']={'img':255-np.clip(self.fimgs['bgr'][:,:,1]-((self.fimgs['bgr'][:,:,0]+self.fimgs['bgr'][:,:,2])/2),0,255)}
+        self.masks['sp_red']={'img':255-np.clip(self.fimgs['bgr'][:,:,2]-((self.fimgs['bgr'][:,:,1]+self.fimgs['bgr'][:,:,0])/2),0,255)}
 
-        combined = overlay(combined,lp_image)
-        combined = overlay(combined,sd_image)
-        #cv2.imshow("shapes", combined)
-
-        print("Running at "+ str(1/(rospy.get_time() -self.last_time)) + " Hz")
-        self.last_time = rospy.get_time()
-        #cv2.waitKey(1)
-
-        self.pub.publish(self.bridge.cv2_to_imgmsg(combined, "bgr8"))
+        # contour finding
+        for mask in self.masks:
+            self.masks[mask]['cnts']=getContoursFromMask(self.masks[mask]['img'])
+        
+        # run the shape identifier
+        # desired result: a single shape.
+        result=runOperation(self.masks,{
+            'sp_blue':'blue',
+            'sp_green':'green',
+            'sp_red':'red',
+            'sat_blue':'blue',
+            'sat_green':'green',
+            'sat_red':'red'
+            },shapes.identify,shapes.collate)
+        rospy.loginfo(result)
+        
+        # run the light pattern identifier
+        # desired result: the current light pattern.
+        self.lp=runOperation(self.masks,{
+            'sp_blue':'blue',
+            'sp_green':'green',
+            'sp_red':'red',
+            'sat_blue':'blue',
+            'sat_green':'green',
+            'sat_red':'red'
+            },light_buoy.identify,light_buoy.collate)
+        rospy.loginfo(self.lp)
+        
+        # self.pub.publish(self.bridge.cv2_to_imgmsg(combined, "bgr8"))
 
 
 if __name__ == "__main__":
--- scripts/vision_scripts/light_buoy.py
+++ scripts/vision_scripts/light_buoy.py
@@ -2,351 +2,67 @@ import cv2
 import numpy as np
 import time
 
-def detectShape(cnt):
+def identify(cnts):
+    # we only care about vertices
+    results=[]
+    for cnt in cnts:
+        peri = cv2.arcLength(cnt, True)
+        vertices = cv2.approxPolyDP(cnt, 0.03 * peri, True)
+        if len(vertices) == 4:
+            # Using the boundingRect method to calculate the width and height
+            # of the quadrilateral and use this to calculate the aspect ratio
+            x, y, width, height = cv2.boundingRect(vertices)
+            aspectRatio = float(width) / height
 
-    # Initially define the shape as a string 'unknown'
-    shape = 'unknown'
-
-    # Calculate the perimeter of the contour
-    peri = cv2.arcLength(cnt, True)
-
-    # Apply contour approximation and store the result in vertices
-    vertices = cv2.approxPolyDP(cnt, 0.03 * peri, True)
-
-    # If there are 3 vertices, the shape is a triangle
-    if len(vertices) == 3:
-        shape = 'triangle'
-
-    # If there are 4 vertices, the shape is a quadrilateral
-    elif len(vertices) == 4:
-
-        # Using the boundingRect method to calculate the width and height
-        # of the quadrilateral and use this to calculate the aspect ratio
-        x, y, width, height = cv2.boundingRect(vertices)
-        aspectRatio = float(width) / height
-
-        # If the aspectRatio is approximately equal, the shape is a square
-        # Otherwise, the shape is a rectangle
-        if aspectRatio > 1:
-            shape = "not_rectangle"
-        #if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+            # If the aspectRatio is approximately equal, the shape is a square
+            # Otherwise, the shape is a rectangle
+            if aspectRatio > 1:
+                pass
+            #if aspectRatio >= 0.95 and aspectRatio <= 1.05:
             #shape = "rectangle"
-        else:
-            shape = "rectangle"
+            else:
+                results.append({'area':cv2.contourArea(vertices)})
+    return results
 
-    # If there are 5 vertices, the shape is a pentagon
-    elif len(vertices) == 5:
-        shape = "pentagon"
-
-    # Otherwise, assume the shape is a circle
-    else:
-        shape = "circle"
-
-    # Return the shape string
-    return shape
+def collate(results):
+    # update the light pattern with the dominant rectangle.
+    allIDs=[]
+    for color in results: # confidence = area
+        for id in results[color]:
+            # append every item with its color and shape, and area as confidence.
+            allIDs.append({'color':color,'conf':id['area']})
+    # sort items by confidence
+    result=""
+    if len(allIDs):
+        allIDs=sorted(allIDs,key= lambda i: i['conf'],reverse=True)
+        # set result to highest confidence color
+        result=allIDs[0]['color']
+    return lp.update(result)
 
 class LightPattern():
     def __init__(self):
-        self.sequence = ["_","_","_"]
+        self.sequence = []
         self.position = 0
+        self.timeout=10
         self.timeout_counter = 0
-        self.last_sequence = ["_","_","_"]
+        self.last_sequence = []
 
-    def getPattern(self,image):
-        frame = image.copy()
-
-        pattern = ""
-        colour = ""
-        counter = 0
-        tempPattern = ""
-        samecolFlag = 0
-        timeStarted = 0
-        timeout = 4
-        area_lim = 50
-        #Code
-        # Blur frame to increase accuracy
-        blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-
-        # Convert the colours from BGR to HSV format
-        hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
-        height,width,_ = frame.shape
-        blank_image = np.zeros((height,width,3), np.uint8)
-        # Define the HSV colour boundaries
-
-
-        use_simple = False
-        if use_simple:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret , mask = cv2.threshold(gray,220,255,cv2.THRESH_BINARY)
-            cv2.imshow("mask",mask)
-
-        else:
-            lower_blue = np.array([76, 86, 0])
-            upper_blue = np.array([135, 255, 255])
-
-            # Apply a mask to the frame
-            maskB = cv2.inRange(hsv, lower_blue, upper_blue)
-            flframe=frame.astype(float)
-            blue=flframe[:,:,0]-(flframe[:,:,2]+flframe[:,:,1])/2
-            np.clip(blue,0,255)
-            cv2.inRange(blue,np.max(blue)*0.5,255)
-            #cv2.imshow('bl',blue)
-            # Obtain the contours of the masked image
-            _, contoursB, _ = cv2.findContours(maskB, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-            # Define the HSV colour boundaries
-            lower_green = np.array([46, 86, 0])
-            upper_green = np.array([75, 255, 255])
-
-            # Apply a mask to the frame
-            maskG = cv2.inRange(hsv, lower_green, upper_green)
-
-            # Obtain the contours of the masked image
-            _, contoursG, _ = cv2.findContours(maskG, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-            # Define the HSV colour boundaries
-            lower_red1 = np.array([0, 86, 0])
-            upper_red1 = np.array([15, 255, 255])
-
-            # Apply a mask to the frame
-            maskR1 = cv2.inRange(hsv, lower_red1, upper_red1)
-
-            # Obtain the contours of the masked image
-            _, contoursR1, _ = cv2.findContours(maskR1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-
-            # Define the HSV colour boundaries
-            lower_red2 = np.array([166, 86, 0])
-            upper_red2 = np.array([180, 255, 255])
-
-            # Apply a mask to the frame
-            maskR2 = cv2.inRange(hsv, lower_red2, upper_red2)
-
-            # Obtain the contours of the masked image
-            _, contoursR2, _ = cv2.findContours(maskR2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-
-            # Define the HSV colour boundaries
-            lower_yellow = np.array([16, 86, 0])
-            upper_yellow = np.array([45, 255, 255])
-
-            # Apply a mask to the frame
-            maskY = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-            # Obtain the contours of the masked image
-            _, contoursY, _ = cv2.findContours(maskY, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-            # A function to detect the shape of a given contour
-
-            # Loop for all the contours
-            for contour in contoursB:
-
-                # Calculate the area of the contour
-                area = cv2.contourArea(contour)
-                # Call a function to find the shape of the contour
-                shape = detectShape(contour)
-
-                # If the contour is within the range, it may be our desired object
-                if (area > area_lim) & (shape == "rectangle"):
-
-                    # Calculate the moment of the contour
-                    M = cv2.moments(contour)
-
-                    # Using the moment, calculate the centroid of the contour
-                    cX = int(M['m10'] / M['m00'])
-                    cY = int(M['m01'] / M['m00'])
-
-
-                    # Label the contour with the shape name
-                    cv2.putText(blank_image, shape+ str(self.sequence), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (255, 0, 0), 2)
-
-                    colour =  "blue"
-
-                    # Draw the contours onto the object
-                    cv2.drawContours(blank_image, contour, -1, (255, 0, 0), 3)
-
-            # Loop for all the contours
-            for contour in contoursG:
-
-                # Calculate the area of the contour
-                area = cv2.contourArea(contour)
-
-                # Call a function to find the shape of the contour
-                shape = detectShape(contour)
-
-                # If the contour is within the range, it may be our desired object
-                if (area > area_lim) & (shape == "rectangle"):
-
-                    # Calculate the moment of the contour
-                    M = cv2.moments(contour)
-
-                    # Using the moment, calculate the centroid of the contour
-                    cX = int(M['m10'] / M['m00'])
-                    cY = int(M['m01'] / M['m00'])
-
-                    # Label the contour with the shape name
-                    cv2.putText(blank_image, shape + str(self.sequence), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 255, 0), 2)
-
-                    # Draw the contours onto the object
-                    cv2.drawContours(blank_image, contour, -1, (0, 255, 0), 3)
-
-
-                    colour =  "green"
-
-            # Loop for all the contours
-            for contour in contoursR1:
-
-                # Calculate the area of the contour
-                area = cv2.contourArea(contour)
-
-                # Call a function to find the shape of the contour
-                shape = detectShape(contour)
-
-                # If the contour is within the range, it may be our desired object
-                if (area > area_lim) & (shape == "rectangle"):
-
-                    # Calculate the moment of the contour
-                    M = cv2.moments(contour)
-
-                    # Using the moment, calculate the centroid of the contour
-                    cX = int(M['m10'] / M['m00'])
-                    cY = int(M['m01'] / M['m00'])
-
-
-
-                    # Label the contour with the shape name
-                    cv2.putText(blank_image, shape+ str(self.sequence), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 0, 255), 2)
-
-                    # Draw the contours onto the object
-                    cv2.drawContours(blank_image, contour, -1, (0, 0, 255), 3)
-
-                    colour =  "red"
-
-            # Loop for all the contours
-            for contour in contoursR2:
-
-                # Calculate the area of the contour
-                area = cv2.contourArea(contour)
-
-                # Call a function to find the shape of the contour
-                shape = detectShape(contour)
-
-                # If the contour is within the range, it may be our desired object
-                if (area > area_lim) & (shape == "rectangle"):
-
-                    # Calculate the moment of the contour
-                    M = cv2.moments(contour)
-
-                    # Using the moment, calculate the centroid of the contour
-                    cX = int(M['m10'] / M['m00'])
-                    cY = int(M['m01'] / M['m00'])
-
-
-                    # Label the contour with the shape name
-                    cv2.putText(blank_image, shape+ str(self.sequence), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 0, 255), 2)
-
-                    # Draw the contours onto the object
-                    cv2.drawContours(blank_image, contour, -1, (0, 0, 255), 3)
-
-
-                    colour =  "red"
-
-            # Loop for all the contours
-            for contour in contoursY:
-
-                # Calculate the area of the contour
-                area = cv2.contourArea(contour)
-
-                # Call a function to find the shape of the contour
-                shape = detectShape(contour)
-
-                # If the contour is within the range, it may be our desired object
-                if (area > area_lim) & (shape == "rectangle"):
-
-                    # Calculate the moment of the contour
-                    M = cv2.moments(contour)
-
-                    # Using the moment, calculate the centroid of the contour
-                    cX = int(M['m10'] / M['m00'])
-                    cY = int(M['m01'] / M['m00'])
-
-                    # Label the contour with the shape name
-                    cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 0, 255), 2)
-
-                    # Draw the contours onto the object
-                    cv2.drawContours(blank_image, contour, -1, (0, 255, 0), 3)
-
-
-                    colour =  "yellow"
-
-
-        if colour == "":
+    def update(self,color):
+        if color == "":
             self.timeout_counter = self.timeout_counter  + 1
             print(self.timeout_counter)
-            if self.timeout_counter > timeout:
+            if self.timeout_counter > self.timeout:
                 self.position = 0
-                self.sequence = [None,None,None]
+                self.last_sequence = self.sequence
+                self.sequence = []
+                print("Detected sequence:" + self.last_sequence)
         else:
             self.timeout_counter =0
-            if self.position == 0:
-                self.sequence[0] = colour
-                self.position = self.position +1
+            if len(self.sequence) and self.sequence[len(self.sequence)-1] == color:
+                pass
             else:
-                if self.sequence[self.position-1] == colour:
-                    pass
-                else:
-                    self.sequence[self.position] = colour
-                    self.position = self.position +1
-            if self.position > 2:
-                self.position = 2
-                self.last_sequence = self.sequence
-        print(self.sequence)
+                self.sequence.append(color)
+        print("Current sequence:" + " ".join(self.sequence))
+        return self.last_sequence
 
-        #cv2.imshow("Red1",maskR1)
-        #cv2.imshow("Rd2",maskR2)
-        #cv2.imshow("Green",maskG)
-        #cv2.imshow("Yellow",maskY)
-        #cv2.imshow("Blue",maskB)
-
-
-        cv2.putText(blank_image, "Sequence: " + str(self.last_sequence), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        return (blank_image,self.last_sequence)
-
-if __name__ == "__main__":
-    cap = cv2.VideoCapture(1)
-    cap.set(3,640)
-    cap.set(4,480)
-
-    pattern = ""
-    counter = 0
-    tempPattern = ""
-    samecolFlag = 0
-    timeStarted = 0
-    lightpattern = LightPattern()
-
-    while True:
-
-        # Capture a frame of the video
-        _,frame = cap.read()
-        result = lightpattern.getPattern(frame)
-
-        # Display the captured frame
-        cv2.imshow("Frame", result)
-        tempPattern = pattern
-
-
-
-        # Check if the user wants to quit (Press q)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-
-            # Break out of the loop
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+lp=LightPattern()
