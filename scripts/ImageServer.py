b9f2d22e65f18ed6fda9041781578b29c2c00047
acenturyandabit <steeven.liu2@gmail.com>
2018-10-17 13:12:31 +1100

    steven push

---
+++ scripts/ImageServer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np
+# from matplotlib import pyplot as plt
+from rowbot_vision.srv import *
+import rospy
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+import buoy
+import lightbcn
+import shapes
+
+class ImageServer():
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("rowbot/image_raw",Image, self.callback)
+        self.last_time = rospy.get_time()
+        return
+
+    def classify_buoy(self,bearing=0,objInFront=False):
+        ids=buoy.identify(self.image,bearing)
+        types=[]
+        confidences=[]
+        for id in ids:
+            types.push(id.name)
+            confidences.push(id.confidence)
+        return (types, confidences)
+
+    def callback(self,data):
+        #print("Rec image")
+        seconds_start = rospy.get_time()
+        try:
+            self.image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        height, width = self.image.shape[:2]
+        new_w = int(width* 1)
+        new_h = int(height* 1)
+        self.image = cv2.resize(self.image,(new_w, new_h))
+        result=shapes.identify(self.image)
+        rospy.loginfo(result)
+        
+        result=lightbcn.identify(self.image)
+        rospy.loginfo(result)
+
+
+if __name__ == "__main__":
+    rospy.init_node("Image_test")
+    Is = ImageServer()
+    rospy.spin()
--- scripts/shapes.py
+++ scripts/shapes.py
@@ -1,52 +1,80 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
+import filters
 
-def identify(cnts):
-    IDs=[]
-    for c in cnts:
-        # compute the center of the contour, then detect the name of the
-        # shape using only the contour
-        shape = "weird"
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.03 * peri, True)
-        cA=cv2.contourArea(approx)
-        if cA>1 and (cv2.contourArea(c)/cA)<0.95: shape="weird";
-        # Classify ellipses
-        if len(approx)>5 and cv2.isContourConvex(approx):
-            ellipse = cv2.fitEllipse(approx)
-            ellipseArea=ellipse[1][0]*ellipse[1][1]*np.pi/4
-            if (ellipseArea):
-                if np.abs((cA-ellipseArea)/ellipseArea) < 1:
-                    shape = 'circle'
-        # Classify based on vertex count
-        elif len(approx) == 3:
-            shape = "triangle"
 
-        elif len(approx) == 12:
-            shape = "cross"
-        else:
+def identify(img):
+    cnts = filters.getContours(img, ['white'], True)
+    nextCnt=[]
+    rectIndices=[]
+    # get outer level contorus.
+    for c, cnt in enumerate(cnts['white'][0]):
+        # check if they are rectangles
+        # will not work because the contours will be hollow!
+        r = cv2.boundingRect(cnt)
+        nextCnt.append((cnt, r, cnts['white'][1][c]))
+        # append outermost white rectangles
+        if (cnts['white'][1][c][3] == -1):
+            rectIndices.append(c)
+        # if cv2.contourArea(cnt[0])/(r[2]*r[3])<0.9:
+
+    cnts = nextCnt
+    '''cnt
+        [0]:contour
+        [1]:bounding rect
+        [2]:hierarchy
+    '''
+    nextCnt = []
+    for cnt in cnts:
+        # pick out 2nd level contours
+        if cnt[2][3] in rectIndices:
+            #print ("contour parent was outer level")
+            c = cnt[0]
+            # compute the center of the contour, then detect the name of the
+            # shape using only the contour
             shape = "weird"
-        x,y,w,h = cv2.boundingRect(c)
-        rat = float(w)/float(h)
-        cA=cv2.contourArea(c)
-        # check the shape
-        if shape != "weird" and cA > 80 and rat > 0.7 and rat < 1.5 :
-            IDs.append({'area':cA,'cnt':c,'shape':shape,'confidence':cA})
-    #####POST PROCESSSING: confidence based sorting
-    IDs=sorted(IDs,key= lambda i: i['area'],reverse=True)
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.03 * peri, True)
+            cA = cv2.contourArea(approx)
+            if cA > 1 and (cv2.contourArea(c)/cA) < 0.95:
+                shape = "weird"
+            # Classify ellipses
+            if len(approx) > 5 and cv2.isContourConvex(approx):
+                ellipse = cv2.fitEllipse(approx)
+                ellipseArea = ellipse[1][0]*ellipse[1][1]*np.pi/4
+                if (ellipseArea):
+                    if np.abs((cA-ellipseArea)/ellipseArea) < 1:
+                        shape = 'circle'
+            # Classify based on vertex count
+            elif len(approx) == 3:
+                shape = "triangle"
+
+            elif len(approx) == 12:
+                shape = "cross"
+            else:
+                shape = "weird"
+            x, y, w, h = cv2.boundingRect(c)
+            rat = float(w)/float(h)
+            cA = cv2.contourArea(c)
+            col = filters.getContourColor(img, c)
+            # check the shape
+            if shape != "weird" and cA > 80 and rat > 0.7 and rat < 1.5:
+                IDs.append({'area': cA, 'cnt': c, 'shape': shape,
+                            'color': col, 'confidence': cA})
+    # POST PROCESSSING: confidence based sorting
+    IDs = sorted(IDs, key=lambda i: i['area'], reverse=True)
+    # get the colours now.
     return IDs
 
-def collate(results):
-    allIDs=[]
-    for color in results: # confidence = area
-        for id in results[color]:
-            # append every item with its color and shape, and area as confidence.
-            allIDs.append({'name':color+id['shape'],'conf':id['area']})
-    # sort items by confidence
-    if len(allIDs):
-        allIDs=sorted(allIDs,key= lambda i: i['conf'],reverse=True)
-        # return highest confidence shape
-        return allIDs[0]['name']
-    else:
-        return "nothing"
+
+debugMode = "fromFile"
+if __name__ == "__main__":
+    if debugMode == "fromFile":
+        for f in os.listdir(os.path.join(my_path, "shp_tst")):
+            print(f)
+            testimg = cv2.imread(os.path.join(my_path, "shp_tst", f))
+            # print(testimg)
+            r = identify(testimg)
+            print(r)
+            cv2.waitKey(-1)
